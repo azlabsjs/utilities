ec12a17ffc0aa7c56491f4b85a10a263e1a91cbb
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-03-17 17:15:51 +0000

    - Updated build configurations - Update typeof functions namespace -  Added shallowEqual and deepEqual

---
+++ .betterer.ts
@@ -0,0 +1,9 @@
+
+import { typescript } from '@betterer/typescript';
+
+export default {
+  'stricter compilation': () =>
+    typescript('./tsconfig.json', {
+      strict: true,
+    }).include('./src/**/*.ts'),
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  '*.{ts,js}': ['prettier --write', 'eslint --fix'],
+  '*.html': ['prettier --write', 'eslint'],
+  '*.{json,md,css}': ['prettier --write'],
+};
